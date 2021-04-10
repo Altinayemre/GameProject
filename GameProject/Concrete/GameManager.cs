8396ee68b143c8051cf54f3ce068c62231abb511
Emre Altınay <81089561+Altinayemre@users.noreply.github.com>
2021-04-10 23:09:54 +0300

    Add files via upload

---
+++ GameProject/Concrete/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using GameProject.Abstract;
+using GameProject.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Concrete
+{
+    class GameManager : IGameService
+    {
+        public void Add(Game game)
+        {
+            Console.WriteLine(game.GameName+" Adlı oyun sisteme ekledi");
+        }
+
+        public void Delete(Game game)
+        {
+            Console.WriteLine(game.GameName + " Adlı oyun sistemeden silindi");
+        }
+
+        public void Update(Game game)
+        {
+            Console.WriteLine(game.GameName + " Adlı oyun bilgileri güncellendi");
+        }
+    }
+}
