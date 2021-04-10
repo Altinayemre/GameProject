87adce43ef14f517b21d10b554d6795b6289e40f
Emre Altınay <81089561+Altinayemre@users.noreply.github.com>
2021-04-10 23:09:05 +0300

    Add files via upload

---
+++ GameProject/Abstract/IEntityRespository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Abstract
+{
+     interface IEntityRespository<T> where T:class,IEntity,new()
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ GameProject/Abstract/ISaleService.cs
@@ -0,0 +1,13 @@
+﻿using GameProject.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Abstract
+{
+    interface ISaleService
+    {
+        void Sell(Game game, Gamer gamer);
+        void CampaignSell(Gamer gamer, Game game, Campaign campaign);
+    }
+}
